2605fd8ee4a666fd4bdbd2aaa5c480c42db03a44
Michael Pohl <mpohl100@googlemail.com>
2024-03-09 08:05:18 +0000

    partially specialize the calc functions

--- src/par/ContinuableWork.h
+++ src/par/ContinuableWork.h
@@ -7,40 +7,166 @@
 
 namespace par {
 
-template <typename TResult, typename TArgs>
-struct ContinuableWorkImpl : public Work {
-  ContinuableWorkImpl() = default;
-  ContinuableWorkImpl(std::function<TResult(TArgs)> func) : _func{func} {}
-  ContinuableWorkImpl(const ContinuableWorkImpl &) = default;
-  ContinuableWorkImpl(ContinuableWorkImpl &&) = default;
-  ContinuableWorkImpl &operator=(const ContinuableWorkImpl &) = default;
-  ContinuableWorkImpl &operator=(ContinuableWorkImpl &&) = default;
-  virtual ~ContinuableWorkImpl() = default;
+template<typename> class Calc2Impl;
 
-  void call() override { _result = _func(TArgs{}); }
+template <typename TResult, typename TArg>
+struct Calc2Impl<TResult(TArg)> : public Work {
+  Calc2Impl() = default;
+  Calc2Impl(std::function<TResult(TArg)> func) : _func{func} {}
+  Calc2Impl(const Calc2Impl &) = default;
+  Calc2Impl(Calc2Impl &&) = default;
+  Calc2Impl &operator=(const Calc2Impl &) = default;
+  Calc2Impl &operator=(Calc2Impl &&) = default;
+  virtual ~Calc2Impl() = default;
+
+  void call() override { _result = _func(_arg); }
 
   TResult result() const { return _result; }
-
+  TArg arg() const { return _arg; }
+  void set_arg(TArg arg) { _arg = arg; }
 private:
-  std::function<TResult(TArgs)> _func;
+  std::function<TResult(TArg)> _func;
+  TResult _result;
+  TArg _arg;
+};
+
+template<typename>
+class Calc1Impl;
+
+template <typename TResult>
+struct Calc1Impl<TResult()> : public Work {
+  Calc1Impl() = default;
+  Calc1Impl(std::function<TResult()> func) : _func{func} {}
+  Calc1Impl(const Calc1Impl &) = default;
+  Calc1Impl(Calc1Impl &&) = default;
+  Calc1Impl &operator=(const Calc1Impl &) = default;
+  Calc1Impl &operator=(Calc1Impl &&) = default;
+  virtual ~Calc1Impl() = default;
+
+  void call() override { _result = _func(); }
+
+  TResult result() const { return _result; }
+private:
+  std::function<TResult()> _func;
   TResult _result;
 };
 
-template <typename TResult, typename TArgs> struct ContinuableWork {
-  ContinuableWork() : _impl{std::make_shared<ContinuableWorkImpl<TResult, TArgs>>()} {}
-  ContinuableWork(std::function<TResult(TArgs)> func)
-      : _impl{std::make_shared<ContinuableWorkImpl<TResult, TArgs>>(func)} {}
-  ContinuableWork(const ContinuableWork &) = default;
-  ContinuableWork(ContinuableWork &&) = default;
-  ContinuableWork &operator=(const ContinuableWork &) = default;
-  ContinuableWork &operator=(ContinuableWork &&) = default;
-  virtual ~ContinuableWork() = default;
+template <typename TArg>
+struct Calc1Impl<void(TArg)> : public Work {
+  Calc1Impl() = default;
+  Calc1Impl(std::function<void(TArg)> func) : _func{func} {}
+  Calc1Impl(const Calc1Impl &) = default;
+  Calc1Impl(Calc1Impl &&) = default;
+  Calc1Impl &operator=(const Calc1Impl &) = default;
+  Calc1Impl &operator=(Calc1Impl &&) = default;
+  virtual ~Calc1Impl() = default;
+
+  void call() override { _func(_arg); }
+
+  TArg arg() const { return _arg; }
+  void set_arg(TArg arg) { _arg = arg; }
+private:
+  std::function<void(TArg)> _func;
+  TArg _arg;
+};
+
+template<typename>
+class CalcImpl;
+
+template<>
+struct CalcImpl<void()> : public Work {
+  CalcImpl() = default;
+  CalcImpl(std::function<void()> func) : _func{func} {}
+  CalcImpl(const CalcImpl &) = default;
+  CalcImpl(CalcImpl &&) = default;
+  CalcImpl &operator=(const CalcImpl &) = default;
+  CalcImpl &operator=(CalcImpl &&) = default;
+  virtual ~CalcImpl() = default;
+
+  void call() override { _func(); }
+
+private:
+  std::function<void()> _func;
+};
+
+template <typename> struct Calc2;
+
+
+template <typename TResult, typename TArg> 
+struct Calc2<TResult(TArg)> {
+  Calc2() : _impl{std::make_shared<Calc2Impl<TResult(TArg)>>()} {}
+  Calc2(std::function<TResult(TArg)> func)
+      : _impl{std::make_shared<Calc2Impl<TResult(TArg)>>(func)} {}
+  Calc2(const Calc2 &) = default;
+  Calc2(Calc2 &&) = default;
+  Calc2 &operator=(const Calc2 &) = default;
+  Calc2 &operator=(Calc2 &&) = default;
+  virtual ~Calc2() = default;
   Task make_task() const {
     return Task{_impl};
   }
 
 private:
-  std::shared_ptr<ContinuableWorkImpl<TResult, TArgs>> _impl;
+  std::shared_ptr<Calc2Impl<TResult(TArg)>> _impl;
+};
+
+template <typename> struct Calc1;
+
+template <typename TResult> 
+struct Calc1<TResult()> {
+  Calc1() : _impl{std::make_shared<Calc1Impl<TResult()>>()} {}
+  Calc1(std::function<TResult()> func)
+      : _impl{std::make_shared<Calc1Impl<TResult()>>(func)} {}
+  Calc1(const Calc1 &) = default;
+  Calc1(Calc1 &&) = default;
+  Calc1 &operator=(const Calc1 &) = default;
+  Calc1 &operator=(Calc1 &&) = default;
+  virtual ~Calc1() = default;
+  Task make_task() const {
+    return Task{_impl};
+  }
+
+private:
+  std::shared_ptr<Calc1Impl<TResult()>> _impl;
+};
+
+
+template <typename TArg> 
+struct Calc1<void(TArg)> {
+  Calc1() : _impl{std::make_shared<Calc1Impl<void(TArg)>>()} {}
+  Calc1(std::function<void(TArg)> func)
+      : _impl{std::make_shared<Calc1Impl<void(TArg)>>(func)} {}
+  Calc1(const Calc1 &) = default;
+  Calc1(Calc1 &&) = default;
+  Calc1 &operator=(const Calc1 &) = default;
+  Calc1 &operator=(Calc1 &&) = default;
+  virtual ~Calc1() = default;
+  Task make_task() const {
+    return Task{_impl};
+  }
+
+private:
+  std::shared_ptr<Calc1Impl<void(TArg)>> _impl;
+};
+
+template<typename>
+class Calc;
+
+template<>
+struct Calc<void()> {
+  Calc() : _impl{std::make_shared<CalcImpl<void()>>()} {}
+  Calc(std::function<void()> func)
+      : _impl{std::make_shared<CalcImpl<void()>>(func)} {}
+  Calc(const Calc &) = default;
+  Calc(Calc &&) = default;
+  Calc &operator=(const Calc &) = default;
+  Calc &operator=(Calc &&) = default;
+  virtual ~Calc() = default;
+  Task make_task() const {
+    return Task{_impl};
+  }
+private:
+  std::shared_ptr<CalcImpl<void()>> _impl;
 };
 
 } // namespace par
--- tests/continuable_work.cpp
+++ tests/continuable_work.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_all.hpp>
 
 #include "par/ContinuableWork.h"
+#include "par/parallel.h"
+
 
 #include <iostream>
 #include <vector>
@@ -10,8 +12,15 @@ namespace {
 TEST_CASE("ContinuableTask", "[continuable_task]") {
 
   SECTION("ContinuableTaskThenExecutesAllFunctionsOfTheChain") {
-    auto return_function = std::function<int(int)>([](int i) -> int { return i++; });
-    auto work = par::ContinuableWork<int, int>{return_function};
+    auto executor = par::Executor{4};
+    auto return_function = [](){ return 3; };
+    auto work = par::Calc1<int()>{return_function};
+    auto void_function = [](){};
+    auto work2 = par::Calc<void()>{void_function};
+    auto arg_function = [](int x){ };
+    auto work3 = par::Calc1<void(int)>{arg_function};
+    auto inout_function = [](int x){ return x; };
+    auto work4 = par::Calc2<int(int)>{inout_function};
   }
 
 }
