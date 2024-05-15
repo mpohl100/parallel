c5dd5b39eba97424941a4a655bff8b5722e37725
Michael Pohl <mpohl100@googlemail.com>
2024-05-15 17:36:05 +0000

    poll test is now green

--- src/par/Poll.h
+++ src/par/Poll.h
@@ -6,41 +6,42 @@
 
 #include <chrono>
 
-namespace par{
+namespace par {
 
-template <typename TResultWait, typename TResultContinue>
-struct Poll{
-    Poll() = default;
-    Poll(const Poll &) = default;
-    Poll(Poll &&) = default;
-    Poll &operator=(const Poll &) = default;
-    Poll &operator=(Poll &&) = default;
+template <typename TResultWait, typename TResultContinue> struct Poll {
+  Poll() = default;
+  Poll(const Poll &) = default;
+  Poll(Poll &&) = default;
+  Poll &operator=(const Poll &) = default;
+  Poll &operator=(Poll &&) = default;
 
-    Poll(Executor& executor, Calc<TResultWait()> calc_to_wait_for, std::function<TResultContinue(TResultWait)> func_to_continue) : _executor{executor}, _calc_to_wait_for{calc_to_wait_for}, _func_to_continue{func_to_continue} {}
+  Poll(Executor &executor, Calc<TResultWait()> calc_to_wait_for,
+       std::function<TResultContinue(TResultWait)> func_to_continue)
+      : _executor{executor}, _calc_to_wait_for{calc_to_wait_for},
+        _func_to_continue{func_to_continue} {
+    _executor.run(_calc_to_wait_for.make_task());
+  }
 
+  Task make_task() const {
+    return Calc<void()>([this] { poll(); }).make_task();
+  }
 
-    Task make_task() const {
-        return Calc<void()>([this] { poll(); }).make_task();
+  void poll() const {
+    if (!_executor.wait_for(_calc_to_wait_for.make_task(), _timeout)) {
+      auto this_task = make_task();
+      _executor.run(this_task);
+      return;
     }
-
-    void poll() const {
-
-        if (!_executor.wait_for(_calc_to_wait_for.make_task(), _timeout)){
-            auto this_task = make_task();
-            _executor.run(this_task);
-            return;
-        }
-        auto calc_to_continue = Calc<TResultContinue(TResultWait)>([this](TResultWait resultWait){
-            return _func_to_continue(resultWait);
-        });
-        _executor.run(calc_to_continue.make_task());
-    }
+    auto calc_to_continue = Calc<TResultContinue()>(
+        [this]() { return _func_to_continue(_calc_to_wait_for.result()); });
+    _executor.run(calc_to_continue.make_task());
+  }
 
 private:
-    Executor& _executor;
-    Calc<TResultWait()> _calc_to_wait_for;
-    std::function<TResultContinue(TResultWait)> _func_to_continue;
-    std::chrono::microseconds _timeout{50};
+  Executor &_executor;
+  Calc<TResultWait()> _calc_to_wait_for;
+  std::function<TResultContinue(TResultWait)> _func_to_continue;
+  std::chrono::microseconds _timeout{50};
 };
 
-}   // namespace par
+} // namespace par
