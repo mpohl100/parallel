4df35c59787e921743cf65cb741a403c99188429
Michael Pohl <mpohl100@googlemail.com>
2024-05-15 17:36:05 +0000

    poll test is now green

--- tests/poll.cpp
+++ tests/poll.cpp
@@ -2,7 +2,6 @@
 
 #include "par/Poll.h"
 
-
 #include <iostream>
 #include <vector>
 
@@ -11,30 +10,34 @@ namespace {
 TEST_CASE("Poll", "[poll]") {
 
   SECTION("PollFinishes") {
-    auto do_finally = std::function<void(int)>([](int poll_result){
-        std::cout << "Poll finished with result: " << poll_result << std::endl;
-        CHECK(poll_result == 42);
+    bool is_42 = false;
+    auto do_finally = std::function<void(int)>([&is_42](int poll_result) {
+      std::cout << "Poll finished with result: " << poll_result << std::endl;
+      is_42 = poll_result == 42;
     });
     auto executor = par::Executor{4};
     auto func_to_wait_for = std::function<int()>([]() -> int {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        return 42;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      return 42;
     });
-    auto poll = par::Poll{executor, par::Calc<int()>{func_to_wait_for}, do_finally};
+    auto poll =
+        par::Poll{executor, par::Calc<int()>{func_to_wait_for}, do_finally};
     poll.poll();
-    auto fail_finally = std::function<void(int)>([](int fail_result){
-        std::cout << "Fail finished with result: " << fail_result << std::endl;
-        CHECK(fail_result == 31);
-        CHECK(false);
+    bool failed_31 = false;
+    auto fail_finally = std::function<void(int)>([&failed_31](int fail_result) {
+      std::cout << "Fail finished with result: " << fail_result << std::endl;
+      failed_31 = fail_result == 31;
     });
     auto func_to_never_finish = std::function<int()>([]() -> int {
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-        return 31;
+      std::this_thread::sleep_for(std::chrono::seconds(3));
+      return 31;
     });
-    auto poll_fail = par::Poll{executor, par::Calc<int()>{func_to_never_finish}, fail_finally};
+    auto poll_fail = par::Poll{executor, par::Calc<int()>{func_to_never_finish},
+                               fail_finally};
     std::this_thread::sleep_for(std::chrono::seconds(2));
+    CHECK(is_42);
+    CHECK_FALSE(failed_31);
   }
-  
 }
-  
-}   // namespace
+
+} // namespace
