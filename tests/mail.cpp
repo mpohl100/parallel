cd4021e785f0471aebfcaf8d7591d4da4587100e
Michael Pohl <mpohl100@googlemail.com>
2024-06-15 21:53:04 +0000

    implemented test case for mailbox and addressee

--- tests/mail.cpp
+++ tests/mail.cpp
@@ -1,10 +1,10 @@
 #include <catch2/catch_all.hpp>
 
-#include "par/Mail.h"
 #include "par/Executor.h"
-
+#include "par/Mail.h"
 
 #include <iostream>
+#include <functional>
 #include <vector>
 
 namespace {
@@ -12,8 +12,26 @@ namespace {
 TEST_CASE("Mail", "[mail]") {
 
   SECTION("MailCanBeSetup") {
+    bool received = false;
+    const auto on_received = [&received]() { received = true; };
+    struct TestAddressee : public par::AddresseeImpl {
+      TestAddressee(std::function<void()> on_received) : _on_received{on_received} {}
+      void receive() override { _on_received(); }
+      std::function<void()> _on_received;
+    };
+    auto mailbox = par::MailBox{};
+    {
+      auto my_addressee = std::make_shared<TestAddressee>(std::function<void()>{on_received});
+      auto addressee = par::Addressee{my_addressee};
+      addressee.expect(mailbox);
+      mailbox.arrived();
+      CHECK(received);
+      received = false;
+    }
+    // addressee is destructed, so nothing should be received
+    mailbox.arrived();
+    CHECK(!received);
   }
-
 }
 
 } // namespace
