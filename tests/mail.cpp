cf7937bd4a3f16b95de7789ed701f25317cdd937
Michael Pohl <mpohl100@googlemail.com>
2024-06-15 20:59:07 +0000

    setup mail

---
+++ tests/mail.cpp
@@ -0,0 +1,19 @@
+#include <catch2/catch_all.hpp>
+
+#include "par/Mail.h"
+#include "par/Executor.h"
+
+
+#include <iostream>
+#include <vector>
+
+namespace {
+
+TEST_CASE("Mail", "[mail]") {
+
+  SECTION("MailCanBeSetup") {
+  }
+
+}
+
+} // namespace
