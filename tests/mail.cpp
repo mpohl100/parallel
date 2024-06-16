edd8f03b4be6075de395ed7bdc5ee861d530b79d
Michael Pohl <mpohl100@googlemail.com>
2024-06-16 20:43:02 +0000

    renamed to orchestrator

--- tests/mail.cpp
+++ tests/mail.cpp
@@ -14,21 +14,21 @@ TEST_CASE("Mail", "[mail]") {
   SECTION("MailCanBeSetup") {
     bool received = false;
     const auto on_received = [&received]() { received = true; };
-    struct TestAddressee : public par::AddresseeImpl {
-      TestAddressee(std::function<void()> on_received) : _on_received{on_received} {}
+    struct TestOrchestrator : public par::Addressee {
+      TestOrchestrator(std::function<void()> on_received) : _on_received{on_received} {}
       void receive() override { _on_received(); }
       std::function<void()> _on_received;
     };
     auto mailbox = par::MailBox{};
     {
-      auto my_addressee = std::make_shared<TestAddressee>(std::function<void()>{on_received});
-      auto addressee = par::Addressee{my_addressee};
-      addressee.expect(mailbox);
+      auto my_Orchestrator = std::make_shared<TestOrchestrator>(std::function<void()>{on_received});
+      auto Orchestrator = par::Orchestrator{my_Orchestrator};
+      Orchestrator.expect(mailbox);
       mailbox.arrived();
       CHECK(received);
       received = false;
     }
-    // addressee is destructed, so nothing should be received
+    // Orchestrator is destructed, so nothing should be received
     mailbox.arrived();
     CHECK(!received);
   }
